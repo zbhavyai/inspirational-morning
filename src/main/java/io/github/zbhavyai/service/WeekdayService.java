ebec7abaa92a5f811b6dd9a9aece63cb2312c2a5
Bhavyai Gupta <zbhavyai@gmail.com>
2024-06-15 21:54:44 -0600

    add logging for timezone

--- src/main/java/io/github/zbhavyai/service/WeekdayService.java
+++ src/main/java/io/github/zbhavyai/service/WeekdayService.java
@@ -5,10 +5,14 @@ import java.time.ZonedDateTime;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
+import org.jboss.logging.Logger;
+
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class WeekdayService {
+
+    private static final Logger LOGGER = Logger.getLogger(WeekdayService.class.getSimpleName());
 
     public String getWeekDay(String zoneID) {
         ZoneId timeZone;
@@ -18,6 +22,8 @@ public class WeekdayService {
         } catch (Exception e) {
             timeZone = ZoneId.of("America/Edmonton");
         }
+
+        LOGGER.infof("Getting weekday for zone: \"%s\"", timeZone.getId());
 
         ZonedDateTime currentZonedDateTime = ZonedDateTime.now(timeZone);
         return currentZonedDateTime.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
